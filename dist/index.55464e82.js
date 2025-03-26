d642eea70162a3a5b9739b71e720928c11c01adf
zilivan <stroielectromir@mail.ru>
2025-03-26 14:52:30 +0300

    free html

--- dist/index.55464e82.js
+++
@@ -1,6 +0,0 @@
-function onclick__li(element) {
-    click();
-    return element;
-}
-
-//# sourceMappingURL=index.55464e82.js.map
--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import './select/style.scss'
 
 
 let listsInputs = []
